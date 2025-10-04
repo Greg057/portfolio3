102aedb51a6b349130f6512ac632872aa26341cd
Grégoire <88341092+Greg057@users.noreply.github.com>
2025-10-04 18:05:17 +1000

    Deploy Next.js portfolio with GitHub Actions

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/p3' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/p3' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/portfolio3' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio3' : '',
   images: { unoptimized: true },
   trailingSlash: true,
   reactStrictMode: true,
